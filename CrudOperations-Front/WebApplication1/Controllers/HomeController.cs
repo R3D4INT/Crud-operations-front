475187332151c666b18d3c9dc93dca7670e7aadc
R3D4INT <khokhulya2006@gmail.com>
2024-06-13 15:35:30 +0200

    Add localization to .net 4.8 front

--- CrudOperations-Front/WebApplication1/Controllers/HomeController.cs
+++ CrudOperations-Front/WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Helpers;
 
@@ -18,6 +21,7 @@ namespace WebApplication1.Controllers
             ViewBag.Message = ViewHomeMessages.AboutMessage;
 
             return View();
+
         }
 
         public ActionResult Contact()
@@ -31,6 +35,23 @@ namespace WebApplication1.Controllers
             ViewBag.Message = ViewHomeMessages.UserCrudOperationsMessage;
 
             return Redirect(ViewHomeMessages.UserRedirectUrl);
+        }
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            if (lang != "EN" && lang != "IT")
+            {
+                return RedirectToAction("Index");
+            }
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+            var languageCookie = new HttpCookie("Language", lang);
+
+            Response.Cookies.Add(languageCookie);
+
+            return Redirect(Request.Headers["Referer"]);
         }
     }
 }
