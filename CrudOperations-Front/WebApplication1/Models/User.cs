a3931baffc0bf10abbb701b12fe95958fbf98f06
R3D4INT <107789122+R3D4INT@users.noreply.github.com>
2024-06-14 11:39:40 +0200

    Revert "Revert "Crud operations front feature""

---
+++ CrudOperations-Front/WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
